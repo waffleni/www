da8118736ae845bdfc1e938a8019afb15298ed1e
Darwing Medina <darwingjavier31@yahoo.es>
2017-10-10 00:12:11 -0600

    feat(app): footer styles
    footer static content and styles

---
+++ src/components/SiteFooter/index.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+export default () => (
+  <footer className="footer bg-alabaster pt-5">
+    <div className="container container-fluid py-5">
+      <div className="row mb-5">
+        <div className="col-12 col-md-4">
+          <h5 className="text-uppercase mb-4">Waffle Studio</h5>
+          <p className="text-muted">
+            Parturient Nullam Tellus Vehicula Dapibus Ipsum Nullam Bibendum
+          </p>
+          <p className="text-muted">
+            Mollis Tellus Pharetra Malesuada Consectetur Ipsum Nullam Bibendum
+          </p>
+        </div>
+        <div className="col-12 col-md-4">
+          <div className="row">
+            <div className="col-12 col-md-6">
+              <h5 className="text-uppercase mb-4">About</h5>
+              <ul className="list-unstyled">
+                <li className="mb-2">About Us</li>
+                <li className="mb-2">Services</li>
+                <li className="mb-2">Contact</li>
+                <li>Case Studio</li>
+              </ul>
+            </div>
+            <div className="col-12 col-md-6">
+              <h5 className="text-uppercase mb-4">Services</h5>
+              <ul className="list-unstyled">
+                <li className="mb-2">Consulting</li>
+                <li className="mb-2">Web Design</li>
+                <li className="mb-2">App Development</li>
+                <li>Web Development</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-4">
+          <h5 className="text-uppercase strong mb-4">Connect</h5>
+          <ul className="list-unstyled">
+            <li className="d-inline-block mr-4">
+              <a href="//facebook.com" target="__blank">
+                <i className="fa fa-facebook" aria-hidden="true" />
+              </a>
+            </li>
+            <li className="d-inline-block mr-4">
+              <a href="//google.com" target="__blank">
+                <i className="fa fa-google-plus" aria-hidden="true" />
+              </a>
+            </li>
+            <li className="d-inline-block">
+              <a href="//twitter.com" target="__blank">
+                <i className="fa fa-twitter" aria-hidden="true" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="text-center">
+        <span className="text-muted">© 2017 Waffle Studio</span>
+      </div>
+    </div>
+  </footer>
+)
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 
 import SiteNavi from '../components/SiteNavi'
 import SiteCard from '../components/SiteCard'
+import SiteFooter from '../components/SiteFooter'
 
 export default class Index extends React.Component {
   render() {
     return (
-      <div className={'main'}>
-        <SiteNavi title={'Waffle Studio'} {...this.props} />
-        <SiteCard title={'Prueba1'} />
-        <SiteCard title={'Waffle Ni'} />
-        <h1>Waffle Studio</h1>
+      <div>
+        <header>
+          <SiteNavi title={'Waffle Studio'} {...this.props} />
+        </header>
+        <main className="main">
+          <h1>Hello world</h1>
+        </main>
+        <SiteFooter />
       </div>
     )
   }
