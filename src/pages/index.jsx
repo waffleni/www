d10c79d4d7a455c5cc1281b70adc122cd409ee2f
Darwing Medina <darwingjavier31@yahoo.es>
2017-10-12 00:54:10 -0600

    feat(app): Section intro styles and layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Link from 'gatsby-link'
+
 import SiteNavi from '../components/SiteNavi'
 import SiteCard from '../components/SiteCard'
 import SiteFooter from '../components/SiteFooter'
@@ -12,7 +14,30 @@ export default class Index extends React.Component {
           <SiteNavi title={'Waffle Studio'} {...this.props} />
         </header>
         <main className="main">
-          <h1>Hello world</h1>
+          <section className="section section--intro">
+            <div className="container">
+              <div className="row">
+                <div className="col-12 col-md-6 d-block d-md-flex section--intro__col-left">
+                  <h1 className="section--intro__title pl-4 text-uppercase">
+                    Modern Web Desing
+                  </h1>
+                </div>
+                <div className="col-12 col-md-6 d-block d-md-flex">
+                  <div className="call-to-action bg-alabaster mt-auto px-4 py-5">
+                    <p className="h5 mb-4">
+                      Aenean lacinia bibendum nulla sed consectetur.
+                    </p>
+                    <Link
+                      to="/"
+                      className="text-underlined text-dark font-weight-bold"
+                    >
+                      See Our Portafolio
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </main>
         <SiteFooter />
       </div>
